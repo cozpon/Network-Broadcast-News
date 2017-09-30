795e38a403cc16fd2635031a70a4c16c8caf7952
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-09-30 08:59:28 -1000

    Client talking to Server

---
+++ client.js
@@ -0,0 +1,26 @@
+// jshint esversion:6
+const net = require('net');
+const client = new net.connect(6969, '0.0.0.0', () => {
+  client.write("This is Client, saying hello to Server");
+  process.stdin.on('readable', () => {
+    const chunk = process.stdin.read();
+    if(chunk !== null) {
+      client.write(chunk.toString());
+    }
+  });
+});
+  client.on('error', (err) => {
+  throw err;
+});
+
+client.on('data', (chunk) => {
+console.log("Message from Server:", chunk.toString());
+});
+
+client.on('end', () => {
+  console.log("client disconnected");
+});
+
+ // client.setEncoding('utf8');
+ //  client.stdin.on('data', function(chunk) {
+ //  });
---
+++ server.js
@@ -0,0 +1,33 @@
+
+// jshint esversion:6
+const clients = [];
+const net = require('net');
+
+const server = net.createServer((client) => {
+  // server is creating connection to client Socket.
+  client.setEncoding('utf8');
+  client.on('data', (chunk) => {
+    console.log(chunk);
+  });
+
+
+  client.on('end', () => { // on client disconnection
+    console.log('disconnected from server!');
+  });
+  client.pipe(client);
+  client.write('hello from the Server.');
+
+  clients.push(server.address());
+  console.log(clients); // put client information into array
+});
+
+server.on('error', (err) => {
+  throw err;
+});
+
+server.listen(6969, '10.0.1.161', () => {
+  console.log("Server Active, daddio!");
+});
+
+// client.setMaxListeners(infinity);
+ //client.destroy(); // kill client after server's response
